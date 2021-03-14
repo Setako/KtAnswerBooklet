561780daf68544850c597ba1d4998ae8cffb8a31
Setako <zygdtako@outlook.com>
2021-03-14 21:25:02 +0800

    CF R1494

---
+++ src/net/setako/ansbook/codeforces/R1494A.kt
@@ -0,0 +1,39 @@
+import java.util.*
+
+//region Utils Declaration
+private val output = System.out
+private val input = Scanner(System.`in`)
+
+private fun nInt() = input.nextInt()
+private fun nLong() = input.nextLong()
+private fun nDouble() = input.nextDouble()
+private fun nWord() = input.next()
+private fun nBigInteger() = input.nextBigInteger()
+
+private fun nLine() = input.nextLine()
+private fun nLineWords() = nLine().split(" ")
+private fun nLineInts() = nLineWords().map { it.toInt() }
+private fun nLineLongs() = nLineWords().map { it.toLong() }
+private fun nLineDoubles() = nLineWords().map { it.toDouble() }
+private fun nLineBigIntegers() = nLineWords().map { it.toBigInteger() }
+
+private fun w(string: String) = output.print(string)
+private fun w(number: Number) = output.print(number.toString())
+private fun w(arr: Iterable<*>, separator: String = " ") = w(arr.joinToString(separator))
+private fun wl() = output.println()
+private fun wl(string: String) = output.println(string)
+private fun wl(number: Number) = output.println(number.toString())
+private fun wl(arr: Iterable<*>, separator: String = " ") = wl(arr.joinToString(separator))
+private fun wl(vararg arr: Any, separator: String = " ") = wl(arr.joinToString(separator))
+//endregion
+
+fun main() {
+    val t = nLineInts()[0]
+    repeat(t) {
+        val (n, k) = nLineInts()
+        (1..n).filter { it * 2 >= k && it != k }.let {
+            wl(it.size)
+            wl(it)
+        }
+    }
+}
